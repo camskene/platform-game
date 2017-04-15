9b0208391caf6ed4dbd427a0a54bf703b0b99841
Cam Skene <camskene@gmail.com>
2017-04-15 14:30:39 -0400

    Round position values when drawing images.

--- js/main.js
+++ js/main.js
@@ -15,6 +15,8 @@ Hero.prototype.move = function(direction) {
 PlayState = {};
 
 PlayState.init = function() {
+  this.game.renderer.renderSession.roundPixels = true;
+
   this.keys = this.game.input.keyboard.addKeys({
     left: Phaser.KeyCode.LEFT,
     right: Phaser.KeyCode.RIGHT
