837f45cbbaeccec6ed1078d21d523621080e826d
Cam Skene <camskene@gmail.com>
2017-04-15 16:53:24 -0400

    Play sound when hero jumps.

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ Hero.prototype.jump = function() {
   let canJump = this.body.touching.down;
 
   if (canJump) {
-      this.body.velocity.y = -JUMP_SPEED;
+    this.body.velocity.y = -JUMP_SPEED;
   }
 
   return canJump;
@@ -38,7 +38,10 @@ PlayState.init = function() {
   });
 
   this.keys.up.onDown.add(function() {
-    this.hero.jump();
+    let didJump = this.hero.jump();
+    if (didJump) {
+      this.sfx.jump.play();
+    }
   }, this);
 };
 
@@ -52,9 +55,14 @@ PlayState.preload = function() {
   this.game.load.image('grass:2x1', 'images/grass_2x1.png');
   this.game.load.image('grass:1x1', 'images/grass_1x1.png');
   this.game.load.image('hero', 'images/hero_stopped.png');
+  this.game.load.audio('sfx:jump', 'audio/jump.wav');
 };
 
 PlayState.create = function() {
+  this.sfx = {
+    jump: this.game.add.audio('sfx:jump')
+  };
+
   this.game.add.image(0, 0, 'background');
   this._loadLevel(this.game.cache.getJSON('level:1'));
 };
